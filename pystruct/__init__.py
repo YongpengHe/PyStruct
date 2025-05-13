c759813bba9e26e4cd25460828ef0230f4b17dff
Yongpeng He <yongpenghe.scut@gmail.com>
2025-05-13 17:25:45 +0800

    update readme.md

---
+++ pystruct/__init__.py
@@ -0,0 +1,10 @@
+"""
+PyStruct - Fast 3D Structural Analysis Tool
+"""
+
+from .model import Model
+from .geometry import Node, Element, Beam
+from .analysis import Analysis
+from .visualization import Viewer
+
+__author__ = 'Yongpeng He' 
